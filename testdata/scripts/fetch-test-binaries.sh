2762f8124ca3dcc6c41bb4087a8397c91552f420
Identitry <henrik.nilsson@identitry.com>
2021-05-19 10:14:49 +0200

    Update fetch-test-binaries.sh

--- testdata/scripts/fetch-test-binaries.sh
+++ testdata/scripts/fetch-test-binaries.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -fsSL https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.3.2/kubebuilder_2.32._linux_amd64.tar.gz | tar xvz --strip-components=1 -C testdata/
+curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.3.2/kubebuilder_2.3.2_linux_amd64.tar.gz | tar xvz --strip-components=1 -C testdata/
